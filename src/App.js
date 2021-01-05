288b9ab26d5814aa9e2e8feae041fb6721d362e7
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-01-04 20:19:58 -0800

    got some stuff rendering added buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      odometer: '0000',
+    }
+  }
+
+  addClicked(){
+    this.setState({
+      odometer: + "10"
+    });
+  }
+
+  render() {
+    return (
+      <div className="App"> 
+        <Header />
+        <h1>Hello there!!!!!!!!</h1>
+        <Odometer message={this.state.odometer} />
+        <button onClick={this.addOne}>+1</button><button onClick={this.addTen}>+10</button><button onClick={this.addOneHundred}>+100</button><button onClick={this.addOneThousand}>+1000</button>
+        <Footer />
+      </div>
+    )};
+}
+
+
+function Header() {
+  return(
+    <nav> 
+      <h2>this is the header</h2>
+    </nav>
   );
 }
+
+
+function Footer() {
+  return(
+    <footer>
+      <p>this is the footer</p>
+    </footer>
+  );
+}
+
+
+function Odometer(props) {
+  return(
+    <body>
+      <h4>{props.message}</h4>
+      <p>neat</p>
+    </body>
+  );
+}
+
 
 export default App;
